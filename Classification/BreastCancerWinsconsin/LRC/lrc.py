b56a7e14e2bb828b8dd4ebceffd340ba9e4d13a0
heis-en-berg <heisenbergknocked@gmail.com>
2019-11-03 15:36:54 -0500

    Logistic Regression Classifier for first 5 datasets

---
+++ Classification/BreastCancerWinsconsin/LRC/lrc.py
@@ -0,0 +1,47 @@
+import numpy as np
+from sklearn import preprocessing
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+
+# read data (dataset at "https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+(Diagnostic)")
+filename = "../../data/wdbc.data"
+feature_columns_index = np.arange(2,32)
+label_column_index = 1
+X = np.loadtxt(filename, delimiter=',', usecols = feature_columns_index)
+y = np.loadtxt(filename, delimiter=',', usecols = label_column_index, dtype=np.str)
+
+# preprocessing
+unique_labels = np.unique(y)
+le = preprocessing.LabelEncoder()
+le.fit(unique_labels)
+y = le.transform(y)
+
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+# split data into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+# cross validation for hyperparameter tuning
+C = [0.5, 1.0, 2.0]
+solver = ["newton-cg", "lbfgs", "sag", "saga"]
+multi_class = ["ovr", "multinomial"]
+parameters = {
+        'C': C,
+        'solver': solver,
+        'multi_class': multi_class
+        }
+lrc=LogisticRegression(random_state=0, n_jobs=-1)
+grid_search=GridSearchCV(lrc, param_grid=parameters, cv=10, verbose=1, n_jobs=-1)
+grid_search.fit(X_train, y_train)
+
+print("\n\nBest Estimator: " + str(grid_search.best_estimator_))
+print("Score: " + str(grid_search.best_score_))
+print("Best params: " + str(grid_search.best_params_))
+
+# Test Data Accuracy Score
+y_test_pred = grid_search.best_estimator_.predict(X_test)
+print("Test Data Accuracy Score: " + str(accuracy_score(y_test, y_test_pred)))
---
+++ Classification/StatlogGermanCreditData/LRC/lrc.py
@@ -0,0 +1,46 @@
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+'''
+    TODO::not sure which features to use from data
+'''
+# read data (dataset at "https://archive.ics.uci.edu/ml/datasets/Statlog+(German+Credit+Data)")
+filename = "../../data/german.data-numeric"
+data = np.genfromtxt(filename, autostrip=True)
+X = data[:,:20]
+y = data[:,20]
+
+# preprocessing - scaling data and features removal
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+# split data into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+# cross validation for hyperparameter tuning
+C = [0.5, 1.0, 2.0]
+solver = ["newton-cg", "lbfgs", "sag", "saga"]
+multi_class = ["ovr", "multinomial"]
+parameters = {
+        'C': C,
+        'solver': solver,
+        'multi_class': multi_class
+        }
+lrc=LogisticRegression(random_state=0, n_jobs=-1)
+grid_search=GridSearchCV(lrc, param_grid=parameters, cv=10, verbose=1, n_jobs=-1)
+grid_search.fit(X_train, y_train)
+
+print("\n\nBest Estimator: " + str(grid_search.best_estimator_))
+print("Score: " + str(grid_search.best_score_))
+print("Best params: " + str(grid_search.best_params_))
+
+# Test Data Accuracy Score
+y_test_pred = grid_search.best_estimator_.predict(X_test)
+print("Test Data Accuracy Score: " + str(accuracy_score(y_test, y_test_pred)))
+
+print(confusion_matrix(y, grid_search.best_estimator_.predict(X)))
