2ecdb0f0effae9c6328ab58329e2a84f55b87343
heis-en-berg <heisenbergknocked@gmail.com>
2019-11-21 22:23:19 -0500

    grid search cv to random search cv

--- Classification/DiabeticRetinopathy/SVC/svc.py
+++ Classification/DiabeticRetinopathy/SVC/svc.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 
@@ -19,35 +19,35 @@ def applySVC(features_to_be_removed):
     X = data[:,:19]
     y = data[:,19]
     
-    # preprocessing - scaling data and features removal
-    scaler = StandardScaler()
-    X = scaler.fit_transform(X)
-    X = np.delete(X, np.s_[features_to_be_removed], axis=1)
-    
     # split data into training and test set
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
     
-    # cross validation for hyperparameter tuning
-    kernel = ["poly"]#, "rbf"]
-    degree = [5]#np.linspace(1,5,5)
-    gamma = [5]#np.linspace(1,5,5)
-    coef0 = [0]#[0,0.1,1,2,3]
-    parameters = {
-            'kernel': kernel,
-            'degree': degree,
-            'gamma': gamma,
-            'coef0': coef0
-            }
-    svc=SVC(random_state=0)
-    grid_search=GridSearchCV(svc, param_grid=parameters, cv=2, verbose=1, n_jobs=-1)
-    grid_search.fit(X_train, y_train)
+    # preprocessing - scaling data and features removal
+    scaler_train = StandardScaler()
+    X_train = scaler_train.fit_transform(X_train)
+    scaler_test = StandardScaler()
+    X_test = scaler_test.fit_transform(X_test)
+    X_train = np.delete(X_train, np.s_[features_to_be_removed], axis=1)
+    X_test = np.delete(X_test, np.s_[features_to_be_removed], axis=1)
     
-    print("\n\nBest Estimator: " + str(grid_search.best_estimator_))
-    print("Score: " + str(grid_search.best_score_))
-    print("Best params: " + str(grid_search.best_params_))
+    # cross validation for hyperparameter tuning    
+    param_distributions = {
+            'kernel' : ["poly", "rbf"],
+            'degree' : np.linspace(1,5,5),
+            'gamma' : np.linspace(1,5,5),
+            'coef0': [0,0.1,1,2,3]
+    }
+
+    svc=SVC(random_state=0)
+    randcv = RandomizedSearchCV(svc, param_distributions, n_iter=100, verbose=1, random_state=0, cv=5)
+    randcv.fit(X_train, y_train)
+    
+    print("\n\nBest Estimator: " + str(randcv.best_estimator_))
+    print("Score: " + str(randcv.best_score_))
+    print("Best params: " + str(randcv.best_params_))
     
     # Test Data Accuracy Score
-    y_test_pred = grid_search.best_estimator_.predict(X_test)
+    y_test_pred = randcv.best_estimator_.predict(X_test)
     print("Test Data Accuracy Score: " + str(accuracy_score(y_test, y_test_pred)))
 
 
